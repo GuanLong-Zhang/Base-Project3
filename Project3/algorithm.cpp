79f2e14fc106ebcf2712d1a6f5bfedd60e747d15
guanlong zhang <2759819042@qq.com>
2023-11-01 09:39:56 +0800

    add

--- Project3/algorithm.cpp
+++ Project3/algorithm.cpp
@@ -1,11 +1,233 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
+//STL常用算法
+//遍历算法
 
-//int main()
-//{
-//
-//	system("pause");
-//	return 0;
-//}
+//普通函数
+void print_vector(int val)   
+{
+	cout << val << " ";
+}
+
+//仿函数
+class MyPrint
+{
+public:
+	void operator()(int a)
+	{
+		cout << a << " ";
+	}
+};
+
+//for_each()   遍历容器
+void test94()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	for_each(v.begin(), v.end(),print_vector);   //传入普通函数
+	cout << endl;
+	for_each(v.begin(), v.end(), MyPrint());     //传入仿函数
+	cout << endl;
+}
+
+//仿函数
+class Transform
+{
+public:
+	int operator()(int v)    
+	{
+		return v + 100;
+	}
+};
+
+//transform()   将一个容器的内容搬运到另外一个容器
+void test95()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	vector<int>v1;
+	v1.resize(v.size());   //给目标容器开辟空间
+	transform(v.begin(), v.end(), v1.begin(), Transform());    //传入仿函数
+	for_each(v1.begin(), v1.end(), MyPrint());                 //传入仿函数
+	cout << endl;
+}
+
+//查找算法
+
+//find
+void test96()    
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	vector<int>::iterator it = find(v.begin(), v.end(), 50);      //find内置数据类型
+	if (it == v.end())
+	{
+		cout << "没有查找到指定元素" << endl;
+	}
+	else
+	{
+		cout << "查找到了指定元素" << endl;
+		cout << "查找到的元素为：" << *it << endl;
+	}
+
+}
+
+class Person_97
+{
+public:
+	string MyName;
+	int Age;
+	Person_97(string name, int age)
+	{
+		this->MyName = name;
+		this->Age = age;
+	}
+	bool operator==(const Person_97 &p)     //需要对==进行重载,编译器才知道如何对自定义数据类型比较
+	{
+		if (p.Age == this->Age && p.MyName == this->MyName)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+
+void test97()
+{
+	vector<Person_97>v;
+	Person_97 p1("aaa", 10);
+	Person_97 p2("bbb", 20);
+	Person_97 p3("ccc", 30);
+	Person_97 p4("ddd", 40);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+
+	Person_97 pp("bbb", 20);   //待查元素
+	vector<Person_97>::iterator it =  find(v.begin(), v.end(), pp);    //find自定义数据  
+	if (it == v.end())
+	{
+		cout << "没有查找到指定内容" << endl;
+	}
+	else
+	{
+		cout << "查找到了指定内容" << endl;
+		cout << "查找到的内容为：" << (*it).MyName<<" "<<(*it).Age << endl;
+	}
+
+}
+
+//find_if
+class Greater
+{
+public:
+	bool operator()(int a)    //一元谓词
+	{
+		return a > 5;
+	}
+};
+
+void test98()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	//find_if内置数据类型   传入一元谓词   返回第一个满足的元素
+	vector<int>::iterator it = find_if(v.begin(), v.end(), Greater());  
+	if (it == v.end())
+	{
+		cout << "没有查找到指定元素" << endl;
+	}
+	else
+	{
+		cout << "查找到了指定元素" << endl;
+		cout << "查找到的元素为：" << *it << endl;
+	}
+}
+
+class Greater20
+{
+public:
+	bool operator()(Person_97& p)
+	{
+		return p.Age > 20;
+	}
+};
+
+void test99()
+{
+	vector<Person_97>v;
+	Person_97 p1("aaa", 10);
+	Person_97 p2("bbb", 20);
+	Person_97 p3("ccc", 30);
+	Person_97 p4("ddd", 40);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	//find_if内置数据类型   传入一元谓词   返回第一个满足的元素  
+	vector<Person_97>::iterator it = find_if(v.begin(), v.end(), Greater20());     
+	if (it == v.end())
+	{
+		cout << "没有查找到指定元素" << endl;
+	}
+	else
+	{
+		cout << "查找到了指定元素" << endl;
+		cout << "查找到的元素为：" << (*it).MyName << " " << (*it).Age << endl;
+	}
+}
+
+//adjacent_find()   查找相邻重复元素
+void test100()
+{
+	vector<int>v;
+	v.push_back(0);
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(0);
+	v.push_back(3);
+	v.push_back(3);
+	vector<int>::iterator it = adjacent_find(v.begin(), v.end());   //返回相邻重复元素的第一个位置迭代器
+	if (it == v.end())
+	{
+		cout << "没有查找到相邻重复元素" << endl;
+	}
+	else
+	{
+		cout << "查找到了相邻重复元素" << endl;
+		cout << "相邻重复元素为：" << *it << endl;
+	}
+
+}
+int main()
+{
+	//test94();
+	//test95();
+	//test96();
+	//test97();
+	//test98();
+	//test99();
+	test100();
+
+	system("pause");
+	return 0;
+}
--- Project3/function_object.cpp
+++ Project3/function_object.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<algorithm>
+#include<functional>
+
 using namespace std;
 
 //函数对象(仿函数)  仿函数本质是一个类，而不是一个函数,重载小括号
@@ -53,12 +57,138 @@ void test87()
 	doprint(m, " hello c++ !");            //函数对象可以作为参数进行传递
 }
 
-int main()
+//谓词 返回bool类型的仿函数称为谓词
+class greater_five
 {
-	//test85(10,10);
-	//test86(" hello world !");
-	test87();
+public:
+	bool operator()(int a)    //一元谓词
+	{
+		return a > 5;
+	}
+};
 
-	system("pause");
-	return 0;
+void test88()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);   //尾插法
+	}
+	vector<int>::iterator it = find_if(v.begin(), v.end(), greater_five());   //查找第一个满足一元谓词条件的元素,greater_five()为匿名函数对象
+	if (it == v.end())
+	{
+		cout << "未查找到大于5的元素" << endl;
+	}
+	else
+	{
+		cout << "找到大于5的第一个元素为：" << *it << endl;
+	}
 }
+
+void print_vector_result(vector<int> v)
+{
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+	
+}
+
+class MyCompare
+{
+public:
+	bool operator()(int a, int b)    //二元谓词
+	{
+		return a > b;
+	}
+};
+
+void test89()
+{
+	vector<int> v;
+	v.push_back(30);
+	v.push_back(10);
+	v.push_back(40);
+	v.push_back(20);
+	sort(v.begin(), v.end());    //默认升序
+	print_vector_result(v);
+	cout << "-----------------------------" << endl;
+	sort(v.begin(), v.end(), MyCompare());   //降序
+	print_vector_result(v);
+}
+
+//内建函数对象  STL自带的函数对象(仿函数)   需要包含头文件 #include<functional>
+//算数仿函数   一元和二元
+//negate   取反  一元算数仿函数   正数变负数，负数变正数
+void test90()
+{
+	negate<int>v;
+	int a = 10;
+	cout << "初始 a = " << a << endl;
+	int b = v(a);
+	cout << "取反 a = " << b << endl;
+
+}
+
+//plus  加法   二元算数仿函数
+void test91()
+{
+	plus<int>p;
+	int a = 10;
+	int b = 20;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout<< "a + b ="<< p(a, b) << endl;
+	//plus<int>p;     //加 减 乘 除 取模
+	//minus<int>m;      
+	//multiplies<int>m;
+	//divides<int>d;
+	//modulus<int>m;
+
+}
+
+//关系仿函数    等于  不等于  大于  大于等于 小于 小于等于
+void test92()
+{
+	vector<int>v;
+	v.push_back(20);
+	v.push_back(10);
+	v.push_back(40);
+	v.push_back(30);
+	print_vector_result(v);
+	sort(v.begin(), v.end(), greater<int>());   //降序
+	print_vector_result(v);
+
+}
+
+//逻辑仿函数  与 或 非
+void test93()
+{
+	vector<int>v;
+	v.push_back(true);
+	v.push_back(false);
+	v.push_back(true);
+	v.push_back(false);
+	print_vector_result(v);
+	vector<int>v1;
+	v1.resize(v.size());   //在数据搬运之前，需要将目的容器的大小进行指定
+	transform(v.begin(), v.end(), v1.begin(), logical_not<bool>());   //数据搬运
+	print_vector_result(v1);
+}
+
+//int main()
+//{
+//	//test85(10,10);
+//	//test86(" hello world !");
+//	//test87();
+//	//test88();
+//	//test89();
+//	//test90();
+//	//test91();
+//	//test92();
+//	test93();
+//
+//	system("pause");
+//	return 0;
+//}
