8cd4731ee4e39d9e4b0581931d8ad3706bca8810
guanlong zhang <2759819042@qq.com>
2023-11-01 20:40:26 +0800

    add

--- Project3/algorithm.cpp
+++ Project3/algorithm.cpp
@@ -218,16 +218,134 @@ void test100()
 	}
 
 }
-int main()
-{
-	//test94();
-	//test95();
-	//test96();
-	//test97();
-	//test98();
-	//test99();
-	test100();
 
-	system("pause");
-	return 0;
+//binary_search()   二分查找，用于查找有序序列  查找到返回true，查不到返回false
+void test101()
+{
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	int a = 1;  //待查找的元素
+	bool bo = binary_search(v.begin(), v.end(), a);
+	if (bo == true)
+	{
+		cout << "查找到了元素："<< a  << endl;
+	}
+	else
+	{
+		cout << "没有查找到元素：" << a << endl;
+	}
 }
+
+//count()   统计元素个数，返回元素个数
+void test102()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(10);
+	v.push_back(20);
+	int a = 40;    //待统计的元素     统计内置数据类型
+	int num = count(v.begin(), v.end(), a);
+	cout << "待统计的元素" << a << "的个数为：" << num << endl;
+}
+
+void test103()
+{
+	vector<Person_97>v;
+	Person_97 p1("aaa", 10);
+	Person_97 p2("bbb", 20);
+	Person_97 p3("ccc", 30);
+	Person_97 p4("ddd", 40);
+	Person_97 p5("bbb", 20);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+
+	Person_97 pp("bbb", 20);   //待统计元素
+	int num = count(v.begin(), v.end(), pp);    //统计自定义数据    需要在类中进行==的重载，这样编辑器才知道如何比较
+	cout << "姓名为：" << pp.MyName << " " << "年龄为：" << pp.Age << " " << "的元素个数为：" << num << endl;
+	
+}
+
+class Greater_20
+{
+public:
+	bool operator()(int val)   //一元谓词
+	{
+		return val > 20;
+	}
+};
+
+//count_if  按照条件进行统计元素个数  传入谓词
+void test104()
+{
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	v.push_back(30);
+	v.push_back(20);
+	int num = count_if(v.begin(), v.end(), Greater_20());   //统计内置数据类型
+	cout << "大于20的元素个数为：" << num << endl;
+}
+
+class Person_105
+{
+public:
+	bool operator()(Person_97& p)   //一元谓词
+	{
+		if (p.MyName == "bbb" && p.Age == 20)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+
+void test105()
+{
+	vector<Person_97>v;
+	Person_97 p1("aaa", 10);
+	Person_97 p2("bbb", 20);
+	Person_97 p3("ccc", 30);
+	Person_97 p4("ddd", 40);
+	Person_97 p5("bbb", 20);
+	v.push_back(p1);
+	v.push_back(p2);
+	v.push_back(p3);
+	v.push_back(p4);
+	v.push_back(p5);
+	int num = count_if(v.begin(), v.end(), Person_105());   //统计自定义数据类型
+	cout << "姓名为：" << "bbb" << " " << "年龄为：" << 20 << " " << "的元素个数为：" << num << endl;
+
+}
+
+//int main()
+//{
+//	//test94();
+//	//test95();
+//	//test96();
+//	//test97();
+//	//test98();
+//	//test99();
+//	//test100();
+//	//test101();
+//	//test102();
+//	//test103();
+//	//test104();
+//	test105();
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Project3/algorithm1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+//STL常用算法1
+//排序算法
+
